c181837efb9723e80be58e4737b13714563b0cfa
Sher-Kon <sher-kon@ya.ru>
2022-03-22 11:43:35 +0300

    добавил обработчик краш-теста

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,12 @@ app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 app.use(requestLogger); // подключаем логгер запросов до всех обработчиков роутов
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
