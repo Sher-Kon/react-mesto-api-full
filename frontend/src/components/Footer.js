444a4e9401a630e40d185b42c5131302c42783bf
Sher-Kon <sher-kon@ya.ru>
2022-03-24 09:53:30 +0300

    установил версию 1.00

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <div className="footer">
-            <p className="footer__copyright">© 2022 mesto-react   ver 0.76</p>
+            <p className="footer__copyright">© 2022 mesto   ver 1.00</p>
         </div>
     );
 }
