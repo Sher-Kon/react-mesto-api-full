dc2b5657bc12a0f9c83c6529d5fd1ff24c77a47e
Sher-Kon <sher-kon@ya.ru>
2022-03-25 16:10:28 +0300

    JWT хранится на сервере .env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-err'); // 401
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
@@ -12,7 +14,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     next(new UnauthorizedError('Необходима авторизация')); // 401 (токен некорректен)
   }
