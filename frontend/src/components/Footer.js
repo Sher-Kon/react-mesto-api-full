454d0ba6fdad73028dac3328feea07247aef77a5
Sher-Kon <sher-kon@ya.ru>
2022-03-25 16:12:49 +0300

    JWT хранится на сервере .env v1.06

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <div className="footer">
-            <p className="footer__copyright">© 2022 mesto   ver 1.05</p>
+            <p className="footer__copyright">© 2022 mesto   ver 1.06</p>
         </div>
     );
 }
