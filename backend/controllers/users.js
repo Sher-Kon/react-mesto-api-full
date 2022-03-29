527ba2e85e2b58c165e00b28533d05bb939bfaf7
Sher-Kon <sher-kon@ya.ru>
2022-03-29 13:35:32 +0300

    исправил id на _id getUserAuth v1.14

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -10,7 +10,7 @@ const UnauthorizedError = require('../errors/unauthorized-err'); // 401
 const NotFoundError = require('../errors/not-found-err'); // 404
 const ConflictError = require('../errors/conflict-err'); // 409
 
-const { NODE_ENV, JWT_SECRET = 'some-secret-key' } = process.env;
+const { NODE_ENV, JWT_SECRET = 'some-secret-key' } = process.env; // eslint-disable-line
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
@@ -72,7 +72,7 @@ module.exports.createUser = (req, res, next) => {
 module.exports.getUserAuth = (req, res, next) => {
   User.findById(req.user._id)// запрос одного
     .then((users) => {
-      console.dir(users);
+      // console.dir(users); // eslint-disable-line
       if (!users) {
         throw new NotFoundError('Пользователь с указанным _id не найден');// 404
       } else {
@@ -80,7 +80,7 @@ module.exports.getUserAuth = (req, res, next) => {
           about: users.about,
           avatar: users.avatar,
           name: users.name,
-          _id: req.user._id, // _id: users.id,
+          _id: users._id,
         });
       }
     })
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-err'); // 401
 
-const { NODE_ENV, JWT_SECRET='some-secret-key' } = process.env;
+const { NODE_ENV, JWT_SECRET = 'some-secret-key' } = process.env; // eslint-disable-line
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET );
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     next(new UnauthorizedError('Необходима авторизация')); // 401 (токен некорректен)
   }
